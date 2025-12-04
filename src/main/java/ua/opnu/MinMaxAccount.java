99e691f2d81a9eaf3ca3489463806966dfa6467c
Brother147 <105119480+Brother147@users.noreply.github.com>
2025-12-04 22:06:30 +0200

    Create MinMaxAccount.java

---
+++ src/main/java/ua/opnu/MinMaxAccount.java
@@ -0,0 +1,66 @@
+import ua.opnu.java.inheritance.account.BankingAccount;
+import ua.opnu.java.inheritance.account.Credit;
+import ua.opnu.java.inheritance.account.Debit;
+import ua.opnu.java.inheritance.account.Startup;
+
+/**
+ * Банковский счет, который дополнительно хранит
+ * минимальный и максимальный баланс за все время.
+ */
+public class MinMaxAccount extends BankingAccount {
+
+    private int minBalance;
+    private int maxBalance;
+
+    /**
+     * Конструктор: стартовый баланс считается и минимумом, и максимумом.
+     */
+    public MinMaxAccount(Startup s) {
+        super(s);
+        int balance = getBalance();
+        this.minBalance = balance;
+        this.maxBalance = balance;
+    }
+
+    /**
+     * Операция пополнения (debit) с обновлением min/max.
+     */
+    @Override
+    public void debit(Debit d) {
+        super.debit(d);
+        updateMinMax();
+    }
+
+    /**
+     * Операция списания (credit) с обновлением min/max.
+     */
+    @Override
+    public void credit(Credit c) {
+        super.credit(c);
+        updateMinMax();
+    }
+
+    private void updateMinMax() {
+        int balance = getBalance();
+        if (balance < minBalance) {
+            minBalance = balance;
+        }
+        if (balance > maxBalance) {
+            maxBalance = balance;
+        }
+    }
+
+    /**
+     * @return минимальный баланс за все время.
+     */
+    public int getMin() {
+        return minBalance;
+    }
+
+    /**
+     * @return максимальный баланс за все время.
+     */
+    public int getMax() {
+        return maxBalance;
+    }
+}
